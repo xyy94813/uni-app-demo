3b6123e7d761cd11edf7fabd54e67d942b9c6ba7
RoXoM <xyy94813@sina.com>
2024-04-02 11:09:16 +0800

    feat: 下载进度页面，区分运行平台执行不同的下载逻辑

--- src/pages/download-progress/index.vue
+++ src/pages/download-progress/index.vue
@@ -1,21 +1,23 @@
 <template>
   <view>
+    当前进度：{{ progress.toFixed(2) }} %
+    <u-line-progress :percentage="progress.toFixed(2)" activeColor="green"></u-line-progress>
+  </view>
+  <view>
     <up-text v-if="!!selectedResource" :text="selectedResource"></up-text>
     <up-text v-else type="error" text="请选选中资源"></up-text>
   </view>
   <view>
-    当前进度：{{ progress }}
-    <u-line-progress :percentage="(progress * 100).toFixed(2)" activeColor="green"></u-line-progress>
-  </view>
-  <view>
     <up-button type="primary" @click="showPicker = true">切换资源</up-button>
-    <up-picker :show="showPicker" :columns="resourcePickerCols" @confirm="handleResourceChange"
-      @cancel="showPicker = false" closeOnClickOverlay="true" @close="showPicker = false" />
   </view>
+  <up-picker :show="showPicker" :columns="resourcePickerCols" @confirm="handleResourceChange"
+    @cancel="showPicker = false" closeOnClickOverlay="true" @close="showPicker = false" />
+  <u-toast ref="uToastRef"></u-toast>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue';
+import { injectAPPContext } from '../../components/app-context'
 import sleep from '../../utils/sleep'
 
 const resourceList = [
@@ -29,7 +31,77 @@ const selectedResource = ref<string | null>(null);
 const resourcePickerCols = reactive([resourceList.map((item, index) => `资源 ${index}`)])
 
 const progress = ref(0);
-const handleResourceChange = async (selected) => {
+const safeUpdateProgress = (newVal: number) => {
+  progress.value = Math.min(100, newVal);
+}
+
+const uToastRef = ref<any | null>(null)
+function showToast(params: any) {
+  uToastRef.value.show(params);
+}
+
+type RequestAndUpdateProgress = (reqURL: string) => Promise<void>;
+
+const requestAndUpdateProgressRepo = new Map<string, RequestAndUpdateProgress>();
+requestAndUpdateProgressRepo.set('default', async (reqURL: string) => {
+  let done = false
+  // uni.request 无法监听，只能模拟
+  const updateProgress = async () => {
+    if (done) { return }
+    const newProgress = progress.value + 0.1;
+    safeUpdateProgress(Math.min(95, newProgress))
+    await sleep(50); // 只能模拟 
+    await updateProgress()
+  }
+
+  // 获取请求资源
+  uni.request({
+    url: reqURL,
+    success(res) {
+      done = true;
+      safeUpdateProgress(100)
+    },
+  })
+
+  await updateProgress();
+})
+requestAndUpdateProgressRepo.set('web', async (reqURL: string) => {
+  // fetch 仅能在 web 环境，以下行为微信浏览器下会失效
+  const response = await fetch(reqURL);
+  // 资源总大小
+  const contentLen = parseFloat(response.headers.get('Content-Length') ?? '0')
+  // 基于 reader 获取当前的字节大小
+  const reader = await response.body!.getReader();
+
+  let receivedLength = 0;
+  const updateProgress = async () => {
+    const result = await reader.read();
+    if (result.done) {
+      safeUpdateProgress(100)
+      return
+    }
+    // receivedLength += result.value.length;
+    receivedLength += result.value.byteLength;
+    safeUpdateProgress((receivedLength / contentLen) * 100)
+    await sleep(50)
+    await updateProgress()
+  }
+
+  await updateProgress();
+})
+
+const appContext = injectAPPContext();
+const uniPlatform = appContext.systemInfo.uniPlatform
+
+let requestAndUpdateProgress: RequestAndUpdateProgress;
+if (requestAndUpdateProgressRepo.has(uniPlatform)) {
+  requestAndUpdateProgress = requestAndUpdateProgressRepo.get(uniPlatform)!
+} else {
+  requestAndUpdateProgress = requestAndUpdateProgressRepo.get('default')!
+}
+
+
+const handleResourceChange = async (selected: any) => {
   // 关闭 picker
   showPicker.value = false;
   // 进度重置
@@ -37,61 +109,18 @@ const handleResourceChange = async (selected) => {
 
   selectedResource.value = resourceList[selected.indexs[0]];
 
-  await sleep(2_000) // delay 便于查看效果
-  let done = false
-  const updateProgress = async () => {
-    if (done) { return }
-    await sleep(50); // 只能模拟 
-    const newProgress = progress.value + 0.1;
-    progress.value = Math.min(0.95, newProgress)
-    await updateProgress()
-  }
+  const reqURL = selectedResource.value + '?' + performance.now()
 
-  // 获取请求资源
-  uni.request({
-    url: selectedResource.value + '?' + performance.now(),
-    success(res) {
-      done = true;
-      progress.value = 1;
-    },
-    progressUpdate: (res) => {
-      console.log('onProgressUpdate', res)
-      progress.value = res.progress;
-    }
+  await sleep(1_000) // delay 便于查看效果
+
+  await requestAndUpdateProgress(reqURL)
+
+  showToast({
+    type: 'success',
+    message: '下载完成',
+    icon: false,
   })
 }
-// fetch 仅能在 web 环境，以下行为微信浏览器下会失效
-// const handleResourceChange = async (selected) => {
-//   // 关闭 picker
-//   showPicker.value = false;
-//   // 进度重置
-//   progress.value = 0;
-
-//   await sleep(200) // delay 便于查看效果
-//   // 获取请求资源
-//   const newResourceURL = resourceList[selected.indexs[0]];
-//   const response = await fetch(newResourceURL + '?' + performance.now());
-//   // 资源总大小
-//   const contentLen = parseFloat(response.headers.get('Content-Length') ?? '0')
-//   // 基于 reader 获取当前的字节大小
-//   const reader = await response.body!.getReader();
-
-//   let receivedLength = 0;
-//   const updateProgress = async () => {
-//     const result = await reader.read();
-//     if (result.done) {
-//       progress.value = 1;
-//       return
-//     }
-//     // receivedLength += result.value.length;
-//     receivedLength += result.value.byteLength;
-//     progress.value = receivedLength / contentLen;
-//     await sleep(50)
-//     await updateProgress()
-//   }
-
-//   await updateProgress();
-// }
 </script>
 
 <style></style>
