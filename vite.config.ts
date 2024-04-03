26c03e0108c5a6562149db733d6c717c65766d4d
RoXoM <xyy94813@sina.com>
2024-04-03 17:14:36 +0800

    chore: 指定构建的目标版本
    支付宝小程序不支持 `??` 需要降目标环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
+const ES_TARGET = 'ES2019'; // 支付宝小程序不支持 nullish coalescing operator。该功能是 es2020 标准推出的，但支付宝只支持 es2019。
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [uni()],
+const viteConf = defineConfig({
+  plugins: [
+    uni(),
+  ],
+  build: {
+    target: ES_TARGET,
+  },
+  esbuild: {
+    target: ES_TARGET,
+    tsconfigRaw: {
+      compilerOptions: {
+        target: ES_TARGET,
+      }
+    }
+  }
 });
+
+export default viteConf
