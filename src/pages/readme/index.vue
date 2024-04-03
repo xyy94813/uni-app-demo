3a0f95dc264ec2b65d17faf0681604ceaf6ca292
RoXoM <xyy94813@sina.com>
2024-04-03 17:58:31 +0800

    fix: 小程序不展示 code 内容区

--- src/pages/readme/index.vue
+++ src/pages/readme/index.vue
@@ -1,16 +1,18 @@
 <template>
-    <markdown :content="readeMe"></markdown>
-    <markdown :content="codeHightLight"></markdown>
+  <markdown :content="readeMe"></markdown>
+  <markdown :content="codeHightLight"></markdown>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import readeMe from '../../../README.md?raw'
 import markdown from '../../components/markdown/markdown'
 
-const codeHightLight = `
+// weixin小程序、支付宝小程序必须用 ref 绑定
+const codeHightLight = ref(`
 ---
 
-以下是 code 高亮测试
+#### 以下是 code 高亮测试
 
 \`\`\`js
 const fn = async (a: number): boolean => true;
@@ -19,12 +21,14 @@ const fn = async (a: number): boolean => true;
 ---
 
 \`\`\`yml
-a: a
+version: 3
+
 list:
   - a
   - b
+  
 \`\`\`
-`
+`)
 </script>
 
 <style lang="scss" scoped></style>
